a7f10dfacda15a032154e00b6a491a6b94483045
Tevai34 <117667970+Tevai34@users.noreply.github.com>
2025-04-22 19:55:05 -0400

    Update testblock.py

--- testblock.py
+++ testblock.py
@@ -8,7 +8,6 @@ class TestAverageBlocks(unittest.TestCase):
             [[1, 2], [3, 4]],
             [[5, 6], [7, 8]]
         ]
-        # Each block is one value here:
         expected = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
         result = average_blocks(cube)
         self.assertEqual(result, expected)
